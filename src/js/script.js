72eba6bc71fd1ccf6aaf9fecf24738016abe7bee
Murillo Dourado <devmurillodourado@gmail.com>
2026-06-03 11:08:42 -0300

    feat: funcao do tema light

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,8 @@ document.getElementById('theme-reader').addEventListener('click', () => {
     body.classList.remove('theme-light');
     body.classList.add('theme-reader');
 });
+
+document.getElementById('theme-light').addEventListener('click', () => {
+    body.classList.remove('theme-reader');
+    body.classList.add('theme-light');
+});
