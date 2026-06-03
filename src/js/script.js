17ba376a3c2deda340572fe8328c7b4d6749a381
Murillo Dourado <devmurillodourado@gmail.com>
2026-06-03 11:08:20 -0300

    feat: funcao do tema reader

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,8 @@ const body = document.body;
 document.getElementById('theme-dark').addEventListener('click', () => {
     body.classList.remove('theme-reader', 'theme-light');
 });
+
+document.getElementById('theme-reader').addEventListener('click', () => {
+    body.classList.remove('theme-light');
+    body.classList.add('theme-reader');
+});
