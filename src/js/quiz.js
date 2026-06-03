dbe087092cf32623040f66a31da24da710c994bb
Leonardo Hidifira <yuitileonardo@gmail.com>
2026-06-03 10:58:43 -0300

    feat: Criando a função de mostar a s questões

---
+++ src/js/quiz.js
@@ -0,0 +1,36 @@
+let currentQuestion = 0;
+let correctAnswers = 0;
+
+
+
+
+const showQuestion = () =>{
+    if (questions[currentQuestion]){
+        let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+
+        document.querySelector('.question').innerHTML = q.question;
+        document.querySelector('.options').innerHTML = '';
+
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`
+        }
+
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item =>{
+            item.addEventListener('click', optionClickEvent);
+        })
+        
+    } else{
+       finishQuiz();
+    }
+}
